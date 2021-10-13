4be1aae3ac729443b2c022863b42fa0412e5e68a
Cuong Le <cuongle@Dung-Le-2.local>
2021-10-13 00:34:43 -0400

    deploy

--- __init__.py
+++ __init__.py
@@ -12,40 +12,39 @@ load_dotenv(find_dotenv())
 db = SQLAlchemy()
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-    #  Point SQLAlchemy to your Heroku database
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+#  Point SQLAlchemy to your Heroku database
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    #   Gets rid of a warning
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#   Gets rid of a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    db.init_app(app)
+db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
 
-    from .models import User
+from .models import User
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
-    app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
 
-    app.register_blueprint(main_blueprint)
+app.register_blueprint(auth_blueprint)
 
-    from .music import music as music_blueprint
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
 
-    app.register_blueprint(music_blueprint)
+app.register_blueprint(main_blueprint)
 
-    return app
+from .music import music as music_blueprint
+
+app.register_blueprint(music_blueprint)
